0aa4948bd5c587abb77a6ea0f2bfb4acaeac2e57
Hector Valerio <hector.valerio@gmail.com>
2018-12-22 15:44:34 -0500

    na

--- middleware/error.js
+++ middleware/error.js
@@ -2,7 +2,7 @@ const winston = require('winston')
 
 module.exports = function(err, req,res,next){
     // Log the exception
-    winston.error(err.winston)
+    winston.error(err.message, err)
 
     // types of logging
     // error
