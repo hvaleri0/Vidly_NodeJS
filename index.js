a82c963845354c365128ca3a37f862eaaa57ebb3
Hector Valerio <hector.valerio@gmail.com>
2018-12-26 11:20:09 -0500

    Extracting the Logging Logic

--- index.js
+++ index.js
@@ -1,50 +1,14 @@
-//require('express-async-errors') //Use this instead of middleware async handler with try catch block
-const winston = require('winston')
-require('winston-mongodb');
-
 const config = require('config');
-
-
 const Joi = require('joi');
+
 Joi.objectId = require('joi-objectid')(Joi)
 
 const express = require('express');
 const app = express();
 
+require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
-
-// process.on('uncaughtException', (ex) => {
-//     //console.log('WE GOT AN UNCAUGHT EXCEPTION');
-//     winston.error(ex.message,ex);
-//     process.exit(1); //best practice is to terminate and restart the program 
-// })
-
-// process.on('unhandledRejection', (ex) => {
-//     //console.log('WE GOT AN UNHANDLED REJECTION');
-//     winston.error(ex.message,ex);
-//     process.exit(1); //best practice is to terminate and restart the program 
-// })
-
-//Another way of catching exception and rejections is by using winstons built in library:
-winston.handleExceptions(
-    new winston.transports.File ({
-        filename: 'uncaughtExceptions.log'
-    })
-)
-
-process.on('unhandledRejection', (ex) => {
-    throw ex; //no winston.unhandleRejection library but we can cheat by using the uncaught exception library
-})
-
-winston.add(winston.transports.File, {
-    filename: 'logfile.log'
-});
-
-winston.add(winston.transports.MongoDB, {
-    db: 'mongodb://localhost/vidly',
-    level: 'error' // only errror messages will be logged in database, if you set to  info - > errors, warnings and info will be logged
-});
 
 //throw new Error('Something failed during startup.') // Synchronous exception
 
