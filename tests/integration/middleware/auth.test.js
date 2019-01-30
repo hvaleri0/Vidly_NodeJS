54f72f87b2e78d79c2bb27ee8a627a2025669ee1
Hector Valerio <hector.valerio@gmail.com>
2019-01-29 19:31:54 -0500

    Testing the Auth Middleware

--- tests/integration/middleware/auth.test.js
+++ tests/integration/middleware/auth.test.js
@@ -1,11 +1,11 @@
 const request = require('supertest');
-const {Genre} = require('../../../models/genre')
-const {User} = require('../../../models/user')
-let server
+const {Genre} = require('../../../models/genre');
+const {User} = require('../../../models/user');
+let server;
 
-describe('/api/genres', () => {
-    beforeEach( () => { server = require ('../../../index'); });
-    afterEach( async () => {
+describe('auth middleware', () => {
+    beforeEach(() => { server = require ('../../../index'); });
+    afterEach(async () => {
         server.close();
         await Genre.remove({}); 
     });
@@ -29,6 +29,20 @@ describe('/api/genres', () => {
         const res = await exec ();
 
         expect(res.status).toBe(401);
+    });
+
+    it('should return 400 if token is invalid', async () => {
+        token = null;
+
+        const res = await exec ();
+
+        expect(res.status).toBe(400);
+    });
+
+    it('should return 200 if token is valid', async () => {
+        const res = await exec ();
+
+        expect(res.status).toBe(200);
     });
 
 });
