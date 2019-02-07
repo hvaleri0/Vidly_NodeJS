65141b7ac3e2e1990acc7a5b6a38b4de644c5fa4
Hector Valerio <hector.valerio@gmail.com>
2019-02-07 18:38:09 -0500

    Refactoring the domain logic adding a instance method

--- routes/returns.js
+++ routes/returns.js
@@ -1,5 +1,4 @@
 const Joi = require('joi')
-const moment = require('moment');
 const express = require('express');
 const auth = require('../middleware/auth')
 const validate = require('../middleware/validate')
@@ -15,16 +14,14 @@ router.post('/',[auth, validate(validateReturn)], async (req,res, next) => {
 
     if(rental.dateReturned) return res.status(400).send('Return already processed')
 
-    rental.dateReturned = new Date();
-    const rentalDays = moment().diff(rental.dateOut, 'days');
-    rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
+    rental.return()
     await rental.save();
 
     await Movie.update({ _id: rental.movie._id}, {
         $inc: { numberInStock: 1}
     });    
 
-    return res.status(200).send(rental);
+    return res.send(rental);
 
 });
 
