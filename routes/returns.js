cd5ee687a6d3019768d8042b84bf062ab39e22a0
Hector Valerio <hector.valerio@gmail.com>
2019-02-07 18:23:45 -0500

    Moongose Static Methods

--- routes/returns.js
+++ routes/returns.js
@@ -9,11 +9,7 @@ const router = express.Router();
 
 
 router.post('/',[auth, validate(validateReturn)], async (req,res, next) => {
-
-    const rental = await Rental.findOne({
-        'customer._id': req.body.customerId,
-        'movie._id': req.body.movieId
-    });
+    const rental = await Rental.lookup(req.body.customerId,req.body.movieId);
 
     if(!rental) return res.status(404).send('Rental not found.')
 
